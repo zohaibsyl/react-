314d3747f33188db65ab8d44b47a014456e8389f
zohaibsyl <159802070+zohaibsyl@users.noreply.github.com>
2024-06-29 16:16:29 +0500

    add to all files

---
+++ new react/new react/fristreactapp/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const App=()=>{
+  return(
+    <div>
+      <h1>Hello React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, eius.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, nulla?</p>
+    <img src="./vite.svg" alt="" />
+    <input type="text" placeholder="Enter the value" />
+    <button>submit</button>
+    </div>
+
+  )
+}
+
+
+
+export  {App}
